9e1137364281efc5a3f03a694832e2c3bb774086
TANISHQBEDI <66412510+TANISHQBEDI@users.noreply.github.com>
2022-05-15 14:04:08 +0530

    shalu login page backend

--- index.js
+++ index.js
@@ -112,7 +112,7 @@ app.get('/', (req, res) => {
 })
 
 app.get('/login',(req,res)=>{
-    res.sendFile(path.join(__dirname)+"/public/login/login.html");
+    res.sendFile(path.join(__dirname)+"/public/login/login1.html");
 })
 
 
