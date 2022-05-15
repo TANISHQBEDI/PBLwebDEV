5d48d185763d764b9b97f7deca3109413d1e28de
FastCurious <dhruvi.ag07@gmail.com>
2022-05-15 22:58:37 +0530

    menu page

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ app.set('view engine','ejs');
 
 app.use(express.static(path.join(__dirname + '/public')))
 
-// sql config
+// sql configure
 
 var con = sql.createConnection({
     host: "localhost",
