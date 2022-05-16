0bbfc5a758594819f82b4fc98e9acc08216c8012
TANISHQBEDI <66412510+TANISHQBEDI@users.noreply.github.com>
2022-05-16 13:46:15 +0530

    bale bale

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// Import modules and packages
 const express=require('express');
 const app=express();
 const server = require('http').createServer(app)
@@ -9,7 +10,7 @@ const session = require('express-session')
 var bodyParser = require('body-parser');
 const { dirname } = require('path');
 
-let loginstatus=false;
+let loginstatus = false;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json())
@@ -29,8 +30,8 @@ app.use(express.static(path.join(__dirname + '/public')))
 
 var con = sql.createConnection({
     host: "localhost",
-    user: "root",
-    password: "agrawal05",
+    user: "TaDi",
+    password: "TURNIPE@RTh387",
     database: "pbl",
     insecureAuth: true
 });
@@ -116,7 +117,7 @@ app.get('/login',(req,res)=>{
 })
 
 
-
+//login dom manipulation
 app.get('/sign-in', (req, res) => {
     let username = req.query.username;
     let password = req.query.password;
