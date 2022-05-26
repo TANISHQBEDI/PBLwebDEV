5ff5d5ab0e4196d3709ee5ae7b0386f3e200afe4
FastCurious <dhruvi.ag07@gmail.com>
2022-05-26 21:58:19 +0530

    Update index.js

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ app.use(express.static(path.join(__dirname + '/public')))
 var con = sql.createConnection({
     host: "localhost",
     user: "root",
-    password: "8624@SHaLu",
+    password: "agrawal05",
     database: "pbl",
     insecureAuth: true
 });
